e2a7a2723dc5db4f2751476bb873a5aca061443a
Ashley Banks <ashleysmbanks89@gmail.com>
2015-04-20 16:43:27 +0100

    cleared up indentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,28 +64,28 @@
 		<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 			<script>
 			Stripe.setPublishableKey('{{ env("STRIPE_API_PUB_SECRET") }}');
-				jQuery(function($) {
-					$('#subscription-form').submit(function(event) {
-						
-					    var $form = $(this);
-					    $form.find('button').prop('disabled', true);
+			jQuery(function($) {
+				$('#subscription-form').submit(function(event) {
+					
+				    var $form = $(this);
+				    $form.find('button').prop('disabled', true);
 
-					    console.log($form)
+				    console.log($form)
 
-					    Stripe.card.createToken($form, stripeResponseHandler);
+				    Stripe.card.createToken($form, stripeResponseHandler);
 
-					    return false;
-					});
+				    return false;
+				});
 			});
 
 			var stripeResponseHandler = function(status, response) {
-			var $form = $('#subscription-form');
-			console.log(status, response)
-			if (response.error) {
-			    $form.find('.payment-errors').text(response.error.message);
-			    $form.find('button').prop('disabled', false);
-			} else {
-			    var token = response.id;
+				var $form = $('#subscription-form');
+				console.log(status, response)
+				if (response.error) {
+				    $form.find('.payment-errors').text(response.error.message);
+				    $form.find('button').prop('disabled', false);
+				} else {
+				    var token = response.id;
 				    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
 				    $form.get(0).submit();
 				}
